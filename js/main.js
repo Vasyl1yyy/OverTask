a249743f69610514ad93038115dd5e0423cd0a08
Vasyl1yyy <115637018+Vasyl1yyy@users.noreply.github.com>
2024-01-19 15:03:06 +0200

    addTodo

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
 const todo = document.querySelector('.todo');
 const btn = todo.querySelector('.input-btn');
 const input = todo.querySelector('.input-text');
+const list = todo.querySelector('.list');
 
 btn.addEventListener('click', (event) => {
-  console.log(input.value);
+  if (input.value !== '') {
+    console.log(input.value);
+    addTodoList(input.value);
+    input.value = '';
+  }
+});
 
-  input.value = '';
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'Enter') {
+    if (input.value !== '') {
+      console.log(input.value);
+
+      input.value = '';
+    }
+  }
+});
+
+const addTodoList = (text) => {
+  const todoList =
+    '<div class="todo-list"><p>' +
+    text +
+    '</p><button class="list-btn">X</button></div>';
+  todo.querySelector('.list').innerHTML += todoList;
+};
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList == 'list-btn') {
+    list.removeChild('.todo-list');
+  }
 });
