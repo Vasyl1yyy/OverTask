2e6bddbefd57efbfdcf3719457f0caf1cd6c01b2
Vasyl1yyy <115637018+Vasyl1yyy@users.noreply.github.com>
2024-01-19 19:55:59 +0200

    bagdel

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const list = todo.querySelector('.list');
 
 btn.addEventListener('click', (event) => {
   if (input.value !== '') {
-    console.log(input.value);
     addTodoList(input.value);
     input.value = '';
   }
@@ -14,8 +13,7 @@ btn.addEventListener('click', (event) => {
 document.addEventListener('keydown', (event) => {
   if (event.code === 'Enter') {
     if (input.value !== '') {
-      console.log(input.value);
-
+      addTodoList(input.value);
       input.value = '';
     }
   }
@@ -26,11 +24,27 @@ const addTodoList = (text) => {
     '<div class="todo-list"><p>' +
     text +
     '</p><button class="list-btn">X</button></div>';
-  todo.querySelector('.list').innerHTML += todoList;
+  list.innerHTML += todoList;
+  localStorage.setItem('text', text);
 };
 
 document.addEventListener('click', (event) => {
+  const todobtn = list.querySelectorAll('.list-btn');
+  const todolist = list.querySelectorAll('.todo-list');
+
   if (event.target.classList == 'list-btn') {
-    list.removeChild('.todo-list');
+    event.target.classList.toggle('del');
+    for (let i = 0; i < todolist.length; i++) {
+      if (todobtn[i].classList.length == 2) {
+        todolist[i].remove();
+      }
+    }
   }
 });
+
+// Збереження даних в Local Storage
+
+// Отримання даних з Local Storage
+const text = localStorage.getItem('text');
+
+console.log(text);
